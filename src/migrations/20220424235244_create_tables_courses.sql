8ac275c24309b5f06e165b912b75a649d261f03b
tuannguyensn2001 <devpro2001@gmail.com>
2022-04-25 00:51:09 +0700

    update api

--- src/migrations/20220424235244_create_tables_courses.sql
+++ src/migrations/20220424235244_create_tables_courses.sql
@@ -1,19 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE `users` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `username` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL ,
-    `password` varchar(255) NOT NULL,
+CREATE TABLE `courses` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) ,
+    `description` varchar(255) ,
+    `imageUrl` varchar(255) ,
+    `price` decimal(10,2) ,
+    `status` varchar(255),
+    `user_id` int(11),
     `created_at` timestamp NULL DEFAULT NULL,
     `updated_at` timestamp NULL DEFAULT NULL,
     `deleted_at` timestamp NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
-
 )
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE  IF EXISTS `users`
+DROP TABLE IF EXISTS `courses`;
 -- +goose StatementEnd
